7c81bd6dead4e932d3186ad13ce0b51a222fdd1c
tomcrane <tom@nanoplanet.com>
2018-05-18 19:15:37 +0100

    Add hosted manifests for demo

--- app/settings.py
+++
@@ -1,3 +0,0 @@
-import os
-
-EXAMPLE_VARIABLE = os.getenv("EXAMPLE_VARIABLE")
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-nginx -g 'pid /tmp/nginx.pid;'
-
-uwsgi --socket 0.0.0.0:3000 \
-  --plugins python3 \
-  --protocol uwsgi \
-  --module "wsgi"
